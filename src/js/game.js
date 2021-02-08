0a4d9bdfa3868892b0e5ce763e6ee6aaea931773
Georjane <pzgeorjane@gmail.com>
2021-02-08 12:33:05 +0100

    Create scenes

---
+++ src/js/game.js
@@ -0,0 +1,15 @@
+var config = {
+  type: Phaser.WEBGL,
+  width: 480,
+  height: 640,
+  backgroundColor: "black",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [],
+  pixelArt: true,
+  roundPixels: true
+};
