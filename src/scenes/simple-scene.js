730ac2f0aa60527b7e1155b74dd1ee5510cd5fdf
Georjane <pzgeorjane@gmail.com>
2021-02-08 11:42:36 +0100

    Finish setting up webpack

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,5 +1,10 @@
 export class SimpleScene extends Phaser.Scene {
+  preload() {
+    this.load.image('rose', 'assets/rose.jpng');
+  }
+
   create() {
     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    this.add.image(100, 200, 'rose');
   }
 }
