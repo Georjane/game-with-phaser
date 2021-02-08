eb98b75ed0eb207f59763d2f7cc0e0aec2bb452a
Georjane <pzgeorjane@gmail.com>
2021-02-08 11:33:39 +0100

    Configured phaser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import 'phaser';
+
+import { SimpleScene } from './scenes/simple-scene';
+
+const gameConfig = {
+  width: 680,
+  height: 400,
+  scene: SimpleScene
+};
+
+new Phaser.Game(gameConfig);
